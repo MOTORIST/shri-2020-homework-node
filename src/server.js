94375e72043a31ade4005ace51903365eb65bcdf
Gerasin Ivan <webartmotor@gmail.com>
2020-03-16 13:53:58 +0300

    refactor: add no-console in server.js

--- src/server.js
+++ src/server.js
@@ -5,5 +5,6 @@ const { PORT, ENV } = require('./config');
 app.get('/', (_, res) => res.send('Im fine!'));
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server started on port ${PORT} - env (${ENV})`);
 });
