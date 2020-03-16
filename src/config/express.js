97138eecc514f60b9c03f8a6ffd1ffc6014e6e1d
Gerasin Ivan <webartmotor@gmail.com>
2020-03-17 00:37:42 +0300

    feat: add processing of static files from public folder

--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const swagger = require('express-swagger-generator');
 const { ValidationError } = require('express-validation');
 const v1Router = require('../routes/v1');
 const swaggerOptions = require('./swagger');
+const { APP_DIR } = require('.');
 
 const app = express();
 const expressSwagger = swagger(app);
@@ -12,6 +14,7 @@ expressSwagger(swaggerOptions);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/', express.static(path.join(APP_DIR, 'public')));
 app.use('/api/v1', v1Router);
 
 app.use((err, req, res) => {
--- src/server.js
+++ src/server.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const app = require('./config/express');
 const { PORT, ENV } = require('./config');
 
-app.get('/', (_, res) => res.send('Im fine!'));
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server started on port ${PORT} - env (${ENV})`);
