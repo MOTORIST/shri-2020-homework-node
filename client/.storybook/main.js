321fce0d50795140f1bf884ddcb8767c360b3248
Gerasin Ivan <webartmotor@gmail.com>
2020-03-24 19:10:41 +0300

    feat(client): create Theme component

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -4,9 +4,9 @@ module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
+    '@storybook/addon-viewport/register',
     '@storybook/addon-actions',
     '@storybook/addon-links',
-    '@storybook/addon-viewport',
     '@storybook/addon-knobs',
   ],
   webpackFinal: config => {
