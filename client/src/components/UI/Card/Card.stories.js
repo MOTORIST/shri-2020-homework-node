51dd49063c62da94037ae78224ccd6e3b5a3b8a7
Gerasin Ivan <webartmotor@gmail.com>
2020-03-28 19:38:22 +0300

    feat(client): create Card component

---
+++ client/src/components/UI/Card/Card.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import Card from '.';
+import './Card.post.css';
+import './_clickable/Card_clickable.post.css';
+
+export default {
+  title: 'Project',
+  decorators: [withKnobs],
+};
+
+const cardKnobs = () => ({
+  clickable: boolean('Clickable', false),
+});
+
+export const card = () => <Card {...cardKnobs()}>{text('Text', 'Text')}</Card>;
