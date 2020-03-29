ec0a7d49706468bb3b37e8fd090c84bbad3c4458
Gerasin Ivan <webartmotor@gmail.com>
2020-03-30 00:42:40 +0300

    feat(client: Link): add router Link component

--- client/src/components/Project/Link/Link.jsx
+++ client/src/components/Project/Link/Link.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from '../../../libs/classname';
+import { Link as RouteLink } from 'react-router-dom';
 import './Link.post.css';
 
 const LinkCn = cn('Link');
 
-export const Link = ({ href, children, className }) => {
+export const Link = ({ href, to, children, className }) => {
+  if (href) {
+    return (
+      <a className={LinkCn(null, [className])} href={href}>
+        {children}
+      </a>
+    );
+  }
+
   return (
-    <a className={LinkCn(null, [className])} href={href}>
+    <RouteLink className={LinkCn(null, [className])} to={to}>
       {children}
-    </a>
+    </RouteLink>
   );
 };
 
