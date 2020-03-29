33996da1ad767017718ee7bb81d9813fde2c1392
Gerasin Ivan <webartmotor@gmail.com>
2020-03-30 02:03:51 +0300

    feat(client: Code): convert ansi to html

--- client/src/components/UI/Code/Code.jsx
+++ client/src/components/UI/Code/Code.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cn from '../../../libs/classname';
+import Convert from 'ansi-to-html';
 import './Code.post.css';
 
 const CodeCn = cn('Code');
 
 export const Code = ({ children, className }) => {
-  return <pre className={CodeCn(null, [className])}>{children}</pre>;
+  const convert = new Convert({ fg: '#000' });
+
+  return (
+    <pre
+      className={CodeCn(null, [className])}
+      dangerouslySetInnerHTML={{ __html: convert.toHtml(children) }}
+    />
+  );
 };
 
 Code.propTypes = {
