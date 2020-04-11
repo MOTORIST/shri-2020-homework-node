16c26f903092b778672e39c144756f004d242087
Gerasin Ivan <webartmotor@gmail.com>
2020-04-11 19:23:48 +0300

    test(client: IndexPage): add test

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+// Fail tests on any warning
+console.error = message => {
+    throw new Error(message);
+}
