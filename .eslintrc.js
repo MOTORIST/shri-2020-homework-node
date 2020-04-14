93bc10e7bb20b579742fffd33cf4a56e565c4e1c
Gerasin Ivan <webartmotor@gmail.com>
2020-04-14 21:54:44 +0300

    style(client): change eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
+    mocha: true,
   },
   extends: ['airbnb-base'],
   globals: {
@@ -17,5 +18,8 @@ module.exports = {
     'object-curly-newline': 0,
     'no-underscore-dangle': 0,
     'class-methods-use-this': 0,
+    'space-before-function-paren': 0,
+    'func-names': 0,
+    'import/prefer-default-export': 0,
   },
 };
